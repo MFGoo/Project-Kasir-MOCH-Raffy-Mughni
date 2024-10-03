0775f84a25256fccfd4b00e62408751bba40115a
candra delvano <candra@neuronworks.co.id>
2024-10-03 08:44:11 +0700

    Asep

---
+++ resources/views/Test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ku kira ku akan kuat</title>
+</head>
+<body>
+    <H1>Testing!!</H1>
+</body>
+</html>
